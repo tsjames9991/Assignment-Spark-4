22040e17c23e16fd4ae9862d4a0e5e11b7ba4c06
Sudeep James Tirkey <sudeep.james@knoldus.in>
2018-03-07 18:11:08 +0530

    Running in terminal but not in IntelliJ

---
+++ src/main/scala/configuration/package.scala
@@ -0,0 +1,40 @@
+import java.util.Properties
+
+import org.apache.log4j.Logger
+import org.apache.spark.sql.types.{LongType, StringType, StructField, StructType}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Configuration File of the Twitter account.
+  */
+
+package object configuration {
+  val INITIALTIME = 1
+  val FINALTIME = 5
+  val SLIDEITNERVAL = Seconds(INITIALTIME)
+  val WINDOWLENGTH = Seconds(FINALTIME)
+  val Log = Logger.getLogger(this.getClass)
+  val URL = s"jdbc:postgresql://localhost:5432/knoldus"
+  val table = "hashcounts"
+  val sc = new SparkConf().setAppName("TwitterApp").setMaster("local").set("spark.driver.allowMultipleContexts", "true")
+  val ssc = new StreamingContext(sc, SLIDEITNERVAL)
+  val context = new SparkContext("local", "Operations")
+  val prop = new Properties
+  prop.setProperty("driver", "org.postgresql.Driver")
+  prop.getProperty("user", "knoldus")
+  prop.getProperty("password", "knoldus")
+  val struct = StructType(
+    StructField("WindowID", LongType, nullable = false) ::
+      StructField("Tweet", StringType, nullable = false) ::
+      StructField("Count", LongType, nullable = false) :: Nil
+  )
+
+  case class TweetCount(tweet: String,
+                        count: Int
+                       ) {
+    def filter(p: TweetCount => Boolean): TweetCount = ???
+
+    def foreach[U](f: TweetCount => U): Unit = ???
+  }
+}
